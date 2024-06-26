8542591261c323210511301117cc7091ef2c2893
unknown <saidachary95@gmail.com>
2024-06-26 11:29:46 +0530

    changes

--- src/test/java/utilities/TestBase.java
+++ src/test/java/utilities/TestBase.java
@@ -1,7 +1,6 @@
 package utilities;
 
 import java.io.FileInputStream;
-import java.io.IOException;
 import java.time.Duration;
 import java.util.Properties;
 import org.openqa.selenium.PageLoadStrategy;
